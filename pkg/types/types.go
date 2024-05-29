1a906ffeac8a4e499d62c382feaae1a164eca58e
Adolfo García Veytia (Puerco) <puerco@stacklok.com>
2024-05-28 18:33:59 -0600

    Types: Update dep, add ecosystem
    This commit adds the ecosystem type to the types libraries, it also introduces
    a new Ecosystem field in the dependency type.
    
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@stacklok.com>

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -13,12 +13,49 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package types is the collection of main data types used by the Trusty libraries
 package types
+
+// Ecosystem is an identifier of a packaging system supported by Trusty
+type Ecosystem int32
 
 // Dependency represents a generic dependency structure
 type Dependency struct {
-	Name    string
-	Version string
+	Name      string
+	Version   string
+	Ecosystem Ecosystem
+}
+
+const (
+	// ECOSYSTEM_NPM identifies the NPM ecosystem
+	ECOSYSTEM_NPM Ecosystem = 1
+
+	// ECOSYSTEM_GO identifies the Go language
+	ECOSYSTEM_GO Ecosystem = 2
+
+	// ECOSYSTEM_PYPI identifies the Python Package Index
+	ECOSYSTEM_PYPI Ecosystem = 3
+)
+
+// Ecosystems enumerates the supported ecosystems
+var Ecosystems = map[string]Ecosystem{
+	"ECOSYSTEM_NPM":  ECOSYSTEM_NPM,
+	"ECOSYSTEM_GO":   ECOSYSTEM_GO,
+	"ECOSYSTEM_PYPI": ECOSYSTEM_PYPI,
+}
+
+// AsString returns the string representation of the DepEcosystem
+func (ecosystem Ecosystem) AsString() string {
+	switch ecosystem {
+	case ECOSYSTEM_NPM:
+		return "npm"
+	case ECOSYSTEM_GO:
+		return "Go"
+	case ECOSYSTEM_PYPI:
+		return "PyPI"
+	default:
+		return ""
+	}
 }
 
 // ConvertDepsToMap converts a slice of Dependency structs to a map for easier comparison
