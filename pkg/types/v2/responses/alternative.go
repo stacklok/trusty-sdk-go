bcc68f0b6b3f0ea7494aa2d448e483b47905421e
Adolfo García Veytia (puerco) <puerco@stacklok.com>
2024-10-09 20:38:34 -0600

    Add initial reponse types
    This commit adds the initial data structures to capture the following v2/apis:
    
    - v2/alternative
    - v2/license
    - v2/pkg
    - v2/similar
    
    These rely on the objects defined in the previous commits.
    
    Signed-off-by: Adolfo García Veytia (puerco) <puerco@stacklok.com>

---
+++ pkg/types/v2/responses/alternative.go
@@ -0,0 +1,25 @@
+// Copyright 2024 Stacklok, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package responses
+
+import "github.com/stacklok/trusty-sdk-go/pkg/types/v2/objects"
+
+// Example: 'https://api.trustypkg.dev/v2/alternatives?package_name=react&package_type=npm'
+
+// Alternatives captures the response from the v2/alternatives API call.
+type Alternatives struct {
+	Status   *string
+	Packages []*objects.AlternativePackage `json:"packages"`
+}
